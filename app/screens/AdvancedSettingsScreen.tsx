025c3b307cc51b6924f9f0a54adc6707fcb8a186
Darkce <darkce97@gmail.com>
2022-12-03 11:35:29 +0800

    feat: add advanced screen

--- app/screens/AdvancedSettingsScreen.tsx
+++ app/screens/AdvancedSettingsScreen.tsx
@@ -3,9 +3,10 @@ import { observer } from 'mobx-react-lite';
 import { ViewStyle } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { SettingStackParamList } from '@/navigators';
-import { Screen, Text } from '@/components';
+import { Screen, Text, SafeAreaScrollView, ListSection, ListCell, Switch } from '@/components';
 import { useNavigation } from '@react-navigation/native';
-import { useTheme } from '@/theme';
+import { spacing, useTheme } from '@/theme';
+import { TextKeyPath } from '@/i18n';
 
 export const AdvancedSettingsScreen: FC<
   StackScreenProps<SettingStackParamList, 'AdvancedSettings'>
@@ -14,12 +15,56 @@ export const AdvancedSettingsScreen: FC<
   const navigation = useNavigation();
 
   return (
-    <Screen style={$screen}>
-      <Text text="advancedSettings" />
+    <Screen>
+      <SafeAreaScrollView contentContainerStyle={$contentContainer}>
+        <ListSection descriptionTk="advancedSettingsScreen.importImageAfterDeleteTip">
+          <ListCell
+            tk="advancedSettingsScreen.importImageAfterDelete"
+            rightIcon={null}
+            RightAccessory={<Switch />}
+          />
+        </ListSection>
+        <ListSection descriptionTk="advancedSettingsScreen.smartSearchTip">
+          <ListCell
+            tk="advancedSettingsScreen.smartSearch"
+            rightIcon={null}
+            RightAccessory={<Switch />}
+          />
+        </ListSection>
+        <BottomTabVisibleSection />
+      </SafeAreaScrollView>
     </Screen>
   );
 });
 
-const $screen: ViewStyle = {
-  flex: 1,
+const BottomTabVisibleSection = observer(() => {
+  const list: { title: TextKeyPath; value: number }[] = [
+    {
+      title: 'contentNavigator.filesTab',
+      value: 1,
+    },
+    {
+      title: 'contentNavigator.albumTab',
+      value: 1,
+    },
+    {
+      title: 'contentNavigator.moreTab',
+      value: 1,
+    },
+  ];
+
+  return (
+    <ListSection titleTk="advancedSettingsScreen.bottomTabVisible">
+      {list.map((item) => {
+        return (
+          <ListCell key={item.title} tk={item.title} rightIcon={null} RightAccessory={<Switch />} />
+        );
+      })}
+    </ListSection>
+  );
+});
+
+const $contentContainer: ViewStyle = {
+  paddingHorizontal: spacing[6],
+  paddingTop: spacing[8],
 };
